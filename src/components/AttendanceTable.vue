c695ed142cc6a344bac1102b6f2cc5f6288ca0eb
DerZade <DerZade@gmail.com>
2020-05-13 05:48:36 +0200

    Style tweaks

--- src/components/AttendanceTable.vue
+++ src/components/AttendanceTable.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="grad-table">
         <template v-for="(l, i) in lines">
-            <i v-if="l.status === 1" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #66AA66;">check_circle</i>
-            <i v-else-if="l.status === 0" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #c91106;">cancel</i>
-            <i v-else-if="l.status === 0.5" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #d18d1f;">help</i>
-            <i v-else-if="l.status === -1" class="grad-table__status material-icons" :key="`status_${i}`" style="color: lightgrey;">radio_button_unchecked</i>
-            <i v-else class="grad-table__status material-icons" :key="`status_${i}`" style="color: #CCC;">hourglass_empty</i>
-            <span class="grad-table__date" :key="`date_${i}`">{{l.date}}</span>
-            <span class="grad-table__weekday" :key="`weekday_${i}`" v-html="l.weekday"></span>
-            <span class="grad-table__title" :key="`title_${i}`">
-                {{l.title}}
-                <a :key="`link_${i}`" :href="l.link" class="material-icons">link</a>
-            </span>
+            <template v-if="l.type === 'data'">
+                <i v-if="l.status === 1" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #66AA66;">check_circle</i>
+                <i v-else-if="l.status === 0" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #c91106;">cancel</i>
+                <i v-else-if="l.status === 0.5" class="grad-table__status material-icons" :key="`status_${i}`" style="color: #d18d1f;">help</i>
+                <i v-else-if="l.status === -1" class="grad-table__status material-icons" :key="`status_${i}`" style="color: lightgrey;">radio_button_unchecked</i>
+                <i v-else class="grad-table__status material-icons" :key="`status_${i}`" style="color: #CCC;">hourglass_empty</i>
+                <span class="grad-table__date" :key="`date_${i}`" v-html="formatDate(l.date)"></span>
+                <span class="grad-table__title" :key="`title_${i}`">
+                    {{l.title}}
+                    <a :key="`link_${i}`" :href="l.link" class="material-icons" target="_blank">link</a>
+                </span>
+            </template>
+            <template v-else>
+                <span :key="i" class="grad-table__spacer">{{l.title}}</span>
+            </template>
         </template>
     </div>
 </template>
@@ -22,7 +26,11 @@ import ForumTopic from '../services/models/ForumTopic';
 import { FORUM_URI } from '../services/forum';
 
 // 0 = canceled | 1 = firm_commitment | 0.5 = commitment | -1 = no feedback | null = no data loaded
-type Line = { date: string; weekday: string; title: string; link: string; status: 0|1|0.5|-1|null };
+type Line = { type: 'data'; date: Date; title: string; link: string; status: 0|1|0.5|-1|null };
+
+type SpacerLine = { type: 'spacer'; title: string };
+
+const months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
 
 @Component
 export default class AttendanceTableVue extends Vue {
@@ -31,7 +39,7 @@ export default class AttendanceTableVue extends Vue {
     @Prop({ default: () => [], type: Array }) private categories!: number[];
     @Prop({ required: true, type: Number }) private uid!: number;
 
-    private lines: Line[] = [];
+    private lines: (Line|SpacerLine)[] = [];
 
     private created () {
         this.updateLines();
@@ -40,8 +48,10 @@ export default class AttendanceTableVue extends Vue {
     @Watch('topics', { deep: true })
     @Watch('uid')
     @Watch('since')
-    @Watch('categories')
+    @Watch('categories', { deep: true })
     private updateLines () {
+        let lastDate: Date|null = null;
+
         this.lines = this.topics.reduce((acc, cur) => {
             if (this.since < cur.eventDate.getTime() && (this.categories.length === 0 || this.categories.includes(cur.cid))) {
                 let status: 0|1|0.5|-1|null = null;
@@ -51,9 +61,17 @@ export default class AttendanceTableVue extends Vue {
                     status = (val !== undefined) ? val : -1;
                 }
 
+                if (lastDate === null || cur.eventDate.getMonth() !== lastDate.getMonth()) {
+                    lastDate = new Date(cur.eventDate);
+                    acc.push({
+                        type: 'spacer',
+                        title: this.getSpacerText(lastDate)
+                    });
+                }
+
                 acc.push({
-                    date: cur.eventDate.toISOString().substr(0, 10),
-                    weekday: ['So', '<b>Mo</b>', 'Di', 'Mi', 'Do', 'Fr', 'Sa'][cur.eventDate.getDay()],
+                    type: 'data',
+                    date: cur.eventDate,
                     title: cur.titleRaw,
                     link: `${FORUM_URI}/topic/${cur.slug}`,
                     status
@@ -61,7 +79,25 @@ export default class AttendanceTableVue extends Vue {
             }
 
             return acc;
-        }, [] as Line[]);
+        }, [] as (Line|SpacerLine)[]);
+    }
+
+    private formatDate (d: Date): string {
+        const date = `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()}`;
+
+        return [
+            `So, ${date}`,
+            `<b>Mo, ${date}</b>`,
+            `Di, ${date}`,
+            `Mi, ${date}`,
+            `Do, ${date}`,
+            `Fr, ${date}`,
+            `Sa, ${date}`
+        ][d.getDay()];
+    };
+
+    private getSpacerText (d: Date): string {
+        return `${months[d.getMonth()]} ${d.getFullYear().toString().padStart(4, '0')}`;
     }
 }
 </script>
@@ -69,19 +105,27 @@ export default class AttendanceTableVue extends Vue {
 <style lang="scss" scoped>
 .grad-table {
     display: grid;
-    grid-template-columns: [icon] auto [date] auto [weekday] auto [title] 1fr;
-    grid-column-gap: 2rem;
+    grid-template-columns: [icon] auto [date] auto [title] 1fr;
+    grid-column-gap: 1.5rem;
     grid-row-gap: 0.5rem;
     padding-bottom: 4rem;
 
     > * {
-        height: 32px;
+        min-height: 32px;
         display: flex;
         align-items: center;
+        overflow: hidden;
     }
 
     &__title {
         color: rgba(black, 50%);
+    }
+
+    &__spacer {
+        color: rgba(black, 50%);
+        text-transform: uppercase;
+        grid-column: 2 / 4;
+        margin-top: 1rem;
     }
 
     a {
